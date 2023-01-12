9c4976c85a0fd2b7d6614b6c0deed5e57d2e0e25
SanyaOgr <sanya2003ogorodov@gmail.com>
2023-01-12 20:30:19 +0700

    test in main.cpp

--- src/generated/cmake_config.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#define GIT_BRANCH "master"
-#define GIT_COMMIT_HASH "1a8de35"
-/* #undef PROJECT_VERSION_PATCH */
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include "cmake_config.h"
 
 int main()
 {
     auto hi = "Hello";
 
-    std::cout << hi << " World" << std::endl;
+    std::cout << hi << " World " << PROJECT_VERSION_PATCH << std::endl;
 
     return 0;
 }
